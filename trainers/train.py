494444e1975e92a8144dcf02e83e73020f62e5df
ckha2000 <christopher.kha2000@gmail.com>
2022-02-23 03:06:35 +0000

    3.2

--- trainers/train.py
+++ trainers/train.py
@@ -622,16 +622,16 @@ def main():
     # sequence classification model.
 
     # TODO: Huggingface configs.
-    raise NotImplementedError("Please finish the TODO!")
+    config = AutoConfig.from_pretrained('bert-base-uncased') 
 
     # TODO: Tokenizer.
-    raise NotImplementedError("Please finish the TODO!")
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
     # TODO: Defines the model.
     if args.training_phase == "pretrain":
-        raise NotImplementedError("Please finish the TODO!")
+        model = AutoModelForMaskedLM.from_config(config)
     else:
-        raise NotImplementedError("Please finish the TODO!")
+        model = AutoModelForSequenceClassification.from_config(config)
 
     # End of TODO.
     ##################################################
